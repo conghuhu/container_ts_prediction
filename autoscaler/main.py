5ffed20bb2e50d76562d7380b6a8aece92f0046c
conghuhu <congguoqinghuhu@gmail.com>
2024-02-07 14:13:53 +0800

    完成副本数预测算法编写

--- autoscaler/main.py
+++ autoscaler/main.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 import pandas as pd
 from lightgbm import LGBMRegressor
@@ -19,6 +21,7 @@ def process_data():
     replica_data['time'] = pd.to_datetime(replica_data['time'])
     replica_data['hour'] = replica_data['time'].dt.hour
     replica_data['day_of_week'] = replica_data['time'].dt.dayofweek
+    replica_data.sort_values(by=['time'], inplace=True)
 
     # Preprocessing: Drop 'time' column and correct understanding of TOTAL_USAGE columns
     # replica_data_preprocessed = replica_data.drop(columns=['time'])
@@ -40,108 +43,131 @@ def process_data():
     return X_train, X_test, y_train, y_test
 
 
-def train_model(X_train, y_train, model_type='randomForest'):
+def train_model(X_train, y_train, model_type='randomForest', train_mode='grid'):
+    start_time = time.time()
     # Training the Random Forest Regressor
     if model_type == 'randomForest':
-        # 定义要搜索的参数网格
-        # Best parameters: {'max_depth': None, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 500}
-        param_grid = {
-            'n_estimators': [300, 500, 1000],  # 树的数量
-            'max_depth': [None, 10, 20, 30],  # 树的最大深度
-            'min_samples_split': [2, 5, 10],  # 分裂内部节点所需的最少样本数
-            'min_samples_leaf': [1, 2, 4],  # 在叶节点上所需的最小样本数
-        }
-        rd = RandomForestRegressor(random_state=42)
-        # 设置GridSearchCV
-        grid_search = GridSearchCV(estimator=rd, param_grid=param_grid, cv=3, scoring='neg_mean_absolute_error',
-                                   n_jobs=-1, verbose=2)
+        if train_mode == 'grid':
+            # 定义要搜索的参数网格
+            # Best parameters: {'max_depth': None, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 500}
+            param_grid = {
+                'n_estimators': [300, 500, 1000],  # 树的数量
+                'max_depth': [None, 10, 20, 30],  # 树的最大深度
+                'min_samples_split': [2, 5, 10],  # 分裂内部节点所需的最少样本数
+                'min_samples_leaf': [1, 2, 4],  # 在叶节点上所需的最小样本数
+            }
+            rd = RandomForestRegressor(random_state=42)
+            # 设置GridSearchCV
+            grid_search = GridSearchCV(estimator=rd, param_grid=param_grid, cv=3, scoring='neg_mean_absolute_error',
+                                       n_jobs=-1, verbose=2)
 
-        # 进行搜索
-        grid_search.fit(X_train, y_train)
+            # 进行搜索
+            grid_search.fit(X_train, y_train)
 
-        # 输出最佳参数
-        print("Best parameters:", grid_search.best_params_)
-        print("Best score:", -grid_search.best_score_)
+            # 输出最佳参数
+            print("Best parameters:", grid_search.best_params_)
+            print("Best score:", -grid_search.best_score_)
 
-        # 使用最佳参数的模型进行预测
-        rf_model = grid_search.best_estimator_
+            # 使用最佳参数的模型进行预测
+            rf_model = grid_search.best_estimator_
+        else:
+            rf_model = RandomForestRegressor(n_estimators=500, max_depth=None, random_state=42)
+            rf_model.fit(X_train, y_train)
     elif model_type == 'linearRegression':
         rf_model = LinearRegression()
         rf_model.fit(X_train, y_train)
     elif model_type == 'elasticNet':
-        # 定义要搜索的参数网格
-        # Best parameters found:  {'alpha': 1.0, 'l1_ratio': 0.9}
-        param_grid = {
-            'alpha': [0.1, 1.0, 10.0],  # 正则化强度
-            'l1_ratio': [0.1, 0.5, 0.9]  # L1比例在L1和L2正则化之间的混合参数
-        }
-        en = ElasticNet(random_state=42)
-        # Initialize GridSearchCV
-        grid_search = GridSearchCV(estimator=en, param_grid=param_grid, cv=3,
-                                   scoring='neg_mean_squared_error', verbose=2, n_jobs=-1)
+        if train_mode == 'grid':
+            # 定义要搜索的参数网格
+            # Best parameters found:  {'alpha': 1.0, 'l1_ratio': 0.9}
+            param_grid = {
+                'alpha': [0.1, 1.0, 10.0],  # 正则化强度
+                'l1_ratio': [0.1, 0.5, 0.9]  # L1比例在L1和L2正则化之间的混合参数
+            }
+            en = ElasticNet(random_state=42)
+            # Initialize GridSearchCV
+            grid_search = GridSearchCV(estimator=en, param_grid=param_grid, cv=3,
+                                       scoring='neg_mean_squared_error', verbose=2, n_jobs=-1)
 
-        # Fit GridSearchCV
-        grid_search.fit(X_train, y_train)
+            # Fit GridSearchCV
+            grid_search.fit(X_train, y_train)
 
-        # Best parameters and best score
-        print("Best parameters found: ", grid_search.best_params_)
-        print("Best score found: ", grid_search.best_score_)
-        # Use the best parameters to re-train the model
-        rf_model = grid_search.best_estimator_
+            # Best parameters and best score
+            print("Best parameters found: ", grid_search.best_params_)
+            print("Best score found: ", grid_search.best_score_)
+            # Use the best parameters to re-train the model
+            rf_model = grid_search.best_estimator_
+        else:
+            rf_model = ElasticNet(alpha=1.0, l1_ratio=0.9, random_state=42)
+            rf_model.fit(X_train, y_train)
     elif model_type == 'xgboost':
-        # Best parameters found:  {'colsample_bytree': 1, 'early_stopping_rounds': 10, 'learning_rate': 0.01, 'max_depth': 10, 'min_child_weight': 1, 'n_estimators': 500, 'subsample': 0.5}
-        param_grid = {
-            'n_estimators': [400, 500],
-            'max_depth': [i for i in range(3, 11, 2)],
-            'learning_rate': [0.01, 0.05, 0.1],
-            'subsample': [0.5, 0.7, 1],
-            'colsample_bytree': [0.5, 0.7, 1],
-            'early_stopping_rounds': [10],
-            'min_child_weight': [i for i in range(0, 11, 2)],
-            'gamma': [i for i in range(0, 11, 2)],
-            'reg_alpha': [0, 0.5, 1],
-            'reg_lambda': [0, 0.5, 1],
-        }
-        # Initialize the XGBRegressor
-        xgb_regressor = XGBRegressor(random_state=42)
-        # Initialize GridSearchCV
-        grid_search = GridSearchCV(estimator=xgb_regressor, param_grid=param_grid, cv=3,
-                                   scoring='neg_mean_squared_error', verbose=2, n_jobs=-1)
+        if train_mode == 'grid':
+            # Best parameters found:  {'colsample_bytree': 1, 'early_stopping_rounds': 10, 'learning_rate': 0.01, 'max_depth': 10, 'min_child_weight': 1, 'n_estimators': 500, 'subsample': 0.5}
+            param_grid = {
+                'n_estimators': [400, 500],
+                'max_depth': [i for i in range(3, 11, 2)],
+                'learning_rate': [0.01, 0.05, 0.1],
+                'subsample': [0.5, 0.7, 1],
+                'colsample_bytree': [0.5, 0.7, 1],
+                'early_stopping_rounds': [10],
+                'min_child_weight': [i for i in range(0, 11, 2)],
+                'gamma': [i for i in range(0, 11, 2)],
+                'reg_alpha': [0, 0.5, 1],
+                'reg_lambda': [0, 0.5, 1],
+            }
+            # Initialize the XGBRegressor
+            xgb_regressor = XGBRegressor(random_state=42)
+            # Initialize GridSearchCV
+            grid_search = GridSearchCV(estimator=xgb_regressor, param_grid=param_grid, cv=3,
+                                       scoring='neg_mean_squared_error', verbose=2, n_jobs=-1)
 
-        # Fit GridSearchCV
-        grid_search.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
+            # Fit GridSearchCV
+            grid_search.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
 
-        # Best parameters and best score
-        print("Best parameters found: ", grid_search.best_params_)
-        print("Best score found: ", grid_search.best_score_)
-        # Use the best parameters to re-train the model
-        rf_model = grid_search.best_estimator_
+            # Best parameters and best score
+            print("Best parameters found: ", grid_search.best_params_)
+            print("Best score found: ", grid_search.best_score_)
+            # Use the best parameters to re-train the model
+            rf_model = grid_search.best_estimator_
+        else:
+            rf_model = XGBRegressor(n_estimators=500, max_depth=10, learning_rate=0.01, subsample=0.5,
+                                    colsample_bytree=1, min_child_weight=1,
+                                    early_stopping_rounds=10, random_state=42)
+            rf_model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
     elif model_type == 'lightgbm':
-        # Best parameters found:   {'colsample_bytree': 0.75, 'early_stopping_round': 10, 'learning_rate': 0.01, 'max_depth': -1, 'n_estimators': 500, 'num_leaves': 60, 'reg_alpha': 0.01, 'reg_lambda': 0.01, 'subsample': 0.5}
-        param_grid = {
-            'num_leaves': [28, 31, 60],  # Adjust based on dataset size and complexity
-            'max_depth': [3, 5, 10, -1],  # -1 means no limit
-            'learning_rate': [0.1, 0.01, 0.05],
-            'n_estimators': [300, 500, 1000],
-            'early_stopping_round': [10],
-            'subsample': [0.5, 0.8, 1.0],
-            'colsample_bytree': [0.5, 0.75, 1],
-            'reg_alpha': [0, 1e-2, 1e3],
-            'reg_lambda': [0, 1e-2, 1e3],
-        }
-        lgbm = LGBMRegressor(random_state=42)
-        grid_search = GridSearchCV(estimator=lgbm, param_grid=param_grid, cv=3, scoring='neg_mean_squared_error',
-                                   verbose=2, n_jobs=-1)
-        grid_search.fit(X_train, y_train,
-                        eval_set=[(X_test, y_test)])
-        # Best parameters and best score
-        print("Best parameters found: ", grid_search.best_params_)
-        print("Best score found: ", grid_search.best_score_)
+        if train_mode == 'grid':
+            # Best parameters found:   {'colsample_bytree': 0.75, 'early_stopping_round': 10, 'learning_rate': 0.01, 'max_depth': -1, 'n_estimators': 500, 'num_leaves': 60, 'reg_alpha': 0.01, 'reg_lambda': 0.01, 'subsample': 0.5}
+            param_grid = {
+                'num_leaves': [28, 31, 60],  # Adjust based on dataset size and complexity
+                'max_depth': [3, 5, 10, -1],  # -1 means no limit
+                'learning_rate': [0.1, 0.01, 0.05],
+                'n_estimators': [300, 500, 1000],
+                'early_stopping_round': [10],
+                'subsample': [0.5, 0.7, 1],
+                'colsample_bytree': [0.5, 0.75, 1],
+                'reg_alpha': [0, 1e-2, 1, 1e2, 1e3],
+                'reg_lambda': [0, 1e-2, 1, 1e3, 1e3],
+            }
+            lgbm = LGBMRegressor(random_state=42)
+            grid_search = GridSearchCV(estimator=lgbm, param_grid=param_grid, cv=3, scoring='neg_mean_squared_error',
+                                       verbose=2, n_jobs=-1)
+            grid_search.fit(X_train, y_train,
+                            eval_set=[(X_test, y_test)])
+            # Best parameters and best score
+            print("Best parameters found: ", grid_search.best_params_)
+            print("Best score found: ", grid_search.best_score_)
 
-        # Use the best parameters to re-train the model
-        rf_model = grid_search.best_estimator_
+            # Use the best parameters to re-train the model
+            rf_model = grid_search.best_estimator_
+        else:
+            rf_model = LGBMRegressor(n_estimators=500, max_depth=5, learning_rate=0.01, subsample=0.7, num_leaves=60,
+                                     colsample_bytree=0.75, reg_alpha=0.01, reg_lambda=0.01,
+                                     early_stopping_round=10, random_state=42)
+            rf_model.fit(X_train, y_train, eval_set=[(X_test, y_test)])
     else:
         raise Exception(f"Model type {model_type} not supported")
+    end_time = time.time()  # 记录结束时间
+    print(f"模型训练耗时: {end_time - start_time} 秒")
     return rf_model
 
 
@@ -171,6 +197,8 @@ def predict(rf_model):
     predict_data['time'] = pd.to_datetime(predict_data['time'])
     predict_data['hour'] = predict_data['time'].dt.hour
     predict_data['day_of_week'] = predict_data['time'].dt.dayofweek
+    predict_data.sort_values(by=['time'], inplace=True)
+
     predict_X = predict_data[['CPU_TOTAL_USAGE', 'MEM_TOTAL_USAGE', 'hour', 'day_of_week']]
     predict_X_poly = poly.transform(predict_X[['CPU_TOTAL_USAGE', 'MEM_TOTAL_USAGE']])
     predict_X_poly_df = pd.DataFrame(predict_X_poly,
@@ -179,8 +207,11 @@ def predict(rf_model):
     predict_X_poly_df.drop('MEM_TOTAL_USAGE', axis=1, inplace=True)
     predict_X = pd.concat([predict_X.reset_index(drop=True), predict_X_poly_df], axis=1)
 
+    start_time = time.time()
     # Predicting the expected POD_COUNT using the trained model
     predicted_pod_count = rf_model.predict(predict_X)
+    end_time = time.time()
+    print(f"Predicting the expected POD_COUNTs using the trained model took {end_time - start_time} seconds")
 
     # Rounding the predicted POD_COUNTs to the nearest integer
     rounded_predicted_pod_count = np.round(predicted_pod_count).astype(int)
@@ -193,6 +224,7 @@ def predict(rf_model):
 if __name__ == '__main__':
     model_type = 'lightgbm'
     X_train, X_test, y_train, y_test = process_data()
-    model = train_model(X_train, y_train, model_type)
+    model = train_model(X_train, y_train, model_type, 'grid')
+
     test_model(model, X_test, y_test, model_type)
     predict(model)
