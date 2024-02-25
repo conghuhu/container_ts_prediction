6f9ac215b39f95105d53a579c037d7f0651bbe44
conghuhu <congguoqinghuhu@gmail.com>
2024-02-25 13:07:44 +0800

    lstm

--- main_lstm_huawei.py
+++ main_lstm_huawei.py
@@ -1,6 +1,6 @@
 import argparse
 
-from exp.exp_lstm import Exp_LSTM
+from huawei.exp.exp_lstm import Exp_LSTM
 
 parser = argparse.ArgumentParser(description='CNN_LSTM_Attention time series Forecasting')
 
@@ -14,29 +14,29 @@ args = parser.parse_args()
 class Config:
     # basic config
     model_name = 'lstm'  # 模型名称
-    save_path = './checkpoints/{}.pth'.format(model_name)  # 最优模型保存路径
+    save_path = './checkpoints/huawei/{}.pth'.format(model_name)  # 最优模型保存路径
 
     # data loader
-    data_path = './datasets/serverless/data.csv'
+    data_path = './datasets/huawei/data.csv'
     features = 'MS'  # 三个选项M，MS，S。分别是多元预测多元，多元预测单元，单元预测单元
-    target = 'CPU_USAGE'  # 预测目标
+    target = 'total_cpu_usage'  # 预测目标
     checkpoints = args.checkpoints
     scale_type = 'standard'  # 标准化类型 "standard" "minmax"
 
     # forecasting task
-    timestep = 24  # 时间步长，就是利用多少时间窗口
-    output_size = 24  # 多输出任务，最终输出层大小，预测未来几个时间步
-    feature_size = 8  # 每个步长对应的特征数量
+    timestep = 144  # 时间步长，就是利用多少时间窗口
+    output_size = 144  # 多输出任务，最终输出层大小，预测未来几个时间步
+    feature_size = 18  # 每个步长对应的特征数量
     pre_len = output_size  # 预测长度
     inverse = False
 
     # model define
     hidden_size = 64  # 隐层大小
-    num_layers = 3  # RNN的层数
+    num_layers = 2  # RNN的层数
     bidirectional = False
 
     # optimization
-    epochs = 80  # 迭代轮数
+    epochs = 100  # 迭代轮数
     batch_size = 256  # 批次大小
     patience = 5  # 早停机制，如果损失多少个epochs没有改变就停止训练。
     learning_rate = 0.001  # 学习率
@@ -47,7 +47,6 @@ class Config:
     use_gpu = True
     gpu = 0
 
-    train_range = 'all'  # 训练集的范围 ['all', 'train']
     pred_mode = 'paper'  # 预测模式 ['paper', 'show']
     test_show = 'brief'  # 测试集展示 ['all', 'brief']
 
@@ -57,19 +56,18 @@ class Config:
 config = Config()
 
 # setting record of experiments
-setting = 'group_id_{}_ft{}_ts{}_fs{}_os{}_pl{}_epoch{}_lr{}_bs{}_rl{}_hs{}_bi{}_tr{}'.format(config.model_name,
-                                                                                              config.features,
-                                                                                              config.timestep,
-                                                                                              config.feature_size,
-                                                                                              config.output_size,
-                                                                                              config.pre_len,
-                                                                                              config.epochs,
-                                                                                              config.learning_rate,
-                                                                                              config.batch_size,
-                                                                                              config.num_layers,
-                                                                                              config.hidden_size,
-                                                                                              config.bidirectional,
-                                                                                              config.train_range)
+setting = 'huawei_{}_ft{}_ts{}_fs{}_os{}_pl{}_epoch{}_lr{}_bs{}_rl{}_hs{}_bi{}'.format(config.model_name,
+                                                                                       config.features,
+                                                                                       config.timestep,
+                                                                                       config.feature_size,
+                                                                                       config.output_size,
+                                                                                       config.pre_len,
+                                                                                       config.epochs,
+                                                                                       config.learning_rate,
+                                                                                       config.batch_size,
+                                                                                       config.num_layers,
+                                                                                       config.hidden_size,
+                                                                                       config.bidirectional)
 
 config.setting = setting
 exp = Exp_LSTM(config)
