fb5ea3301fd9934e65a6634419acd6e8894fc1b3
conghuhu <congguoqinghuhu@gmail.com>
2024-02-26 20:50:08 +0800

    tmp

--- main_pcnn_lstm_huawei.py
+++ main_pcnn_lstm_huawei.py
@@ -27,7 +27,7 @@ class Config:
     timestep = 145  # 时间步长，就是利用多少时间窗口
     feature_size = 18  # 每个步长对应的特征数量
     output_size = 18  # 多输出任务，最终输出大小，预测未来几个时间步
-    pre_len = 48  # 预测长度
+    pre_len = 144  # 预测长度
     inverse = False
 
     # model define
