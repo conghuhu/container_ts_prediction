c0abd9133b2d917337459b8fc4ba7004828804b6
conghuhu <congguoqinghuhu@gmail.com>
2024-02-27 21:00:05 +0800

    666

--- main_dlinear.py
+++ main_dlinear.py
@@ -26,7 +26,7 @@ class Config:
     # forecasting task
     timestep = 144  # 时间步长，就是利用多少时间窗口
     feature_size = 12  # 每个步长对应的特征数量
-    pre_len = 144  # 预测长度
+    pre_len = 96  # 预测长度
     inverse = False
 
     # model define
