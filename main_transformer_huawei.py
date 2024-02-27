d0b7cfd6dfdf11a9f5410ba7166eb5668ba34976
Wang Guan <wg1024code@gmail.com>
2024-02-27 09:22:42 +0800

    transformer-mlp

--- main_transformer_huawei.py
+++ main_transformer_huawei.py
@@ -41,7 +41,7 @@ class Config:
     dec_type = 'mlp'  # ['decoder', 'mlp']
 
     # optimization
-    epochs = 1  # 迭代轮数
+    epochs = 100  # 迭代轮数
     batch_size = 256  # 批次大小
     patience = 5  # 早停机制，如果损失多少个epochs没有改变就停止训练。
     learning_rate = 0.001  # 学习率
