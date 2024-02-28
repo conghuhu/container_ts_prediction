b70ea2656edea51b20d355135d3d80ea02651c73
conghuhu <congguoqinghuhu@gmail.com>
2024-02-28 13:16:15 +0800

    222

--- main_transformer.py
+++ main_transformer.py
@@ -13,8 +13,8 @@ args = parser.parse_args()
 
 class Config:
     # basic config
-    model_name = 'transformer'  # 模型名称
-    # model_name = 'transformer_mlp'  # 模型名称
+    # model_name = 'transformer'  # 模型名称
+    model_name = 'transformer_mlp'  # 模型名称
     save_path = './checkpoints/{}.pth'.format(model_name)  # 最优模型保存路径
 
     # data loader
@@ -28,18 +28,18 @@ class Config:
     timestep = 144  # 时间步长，就是利用多少时间窗口
     output_size = 12  # 只预测CPU
     feature_size = 12  # 每个步长对应的特征数量（跟数据集处理有关，我只保留了七个特征）
-    pre_len = 96  # 预测长度
+    pre_len = 144  # 预测长度
     inverse = False
 
     # model define
-    hidden_size = 32  # 隐层大小
+    hidden_size = 64  # 隐层大小
     enc_layers = 1  # encoder层数
     dec_layers = 1
     num_heads = 2  # nhead数和d_model也就是嵌入维度必须满足整除关系
     dropout = 0.1
-    forward_expansion = 8
+    forward_expansion = 16
     use_RevIN = False
-    dec_type = 'decoder'  # ['decoder', 'mlp']
+    dec_type = 'mlp'  # ['decoder', 'mlp']
 
     # optimization
     epochs = 100  # 迭代轮数
